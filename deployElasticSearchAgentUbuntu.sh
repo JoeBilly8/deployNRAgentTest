a3f602b64f9c8785dfef42d00adeab67f23c0de6
Joe Moore <45293041+JoeBilly8@users.noreply.github.com>
2021-02-10 13:36:32 +0000

    Create deployElasticSearchAgentUbuntu.sh

---
+++ deployElasticSearchAgentUbuntu.sh
@@ -0,0 +1,27 @@
+# Add the New Relic Infrastructure Agent gpg key \
+
+curl -s https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add - && \
+
+\
+
+# Create a configuration file and add your license key \
+
+echo "license_key: 5c30914d51ee4675aae1a34b10acdc6127b1cfbb" | sudo tee -a /etc/newrelic-infra.yml && \
+
+\
+
+# Create the agent’s yum repository \
+
+printf "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt focal main" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list && \
+
+\
+
+# Update your apt cache \
+
+sudo apt-get update && \
+
+\
+
+# Run the installation script \
+
+sudo apt-get install newrelic-infra -y
